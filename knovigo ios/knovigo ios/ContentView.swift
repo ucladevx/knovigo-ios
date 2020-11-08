484b1988ea5b8246fe24db846b83e88307295233
medhakini <medha.kini@gmail.com>
2020-11-07 23:18:36 -0800

    empty project

---
+++ knovigo ios/knovigo ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  knovigo ios
+//
+//  Created by Medha Kini on 11/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ knovigo ios/knovigo ios/knovigo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  knovigo_iosApp.swift
+//  knovigo ios
+//
+//  Created by Medha Kini on 11/7/20.
+//
+
+import SwiftUI
+
+@main
+struct knovigo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
