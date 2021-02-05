25ae18b60ef92b14e4df0468e87a179601595033
Eric Yang <eric8yang@gmail.com>
2021-02-05 11:45:03 -0800

    updated homepage

--- Knovigo/Controllers/HomeViewController.swift
+++ Knovigo/Controllers/HomeViewController.swift
@@ -10,36 +10,34 @@ import UIKit
 import GoogleMaps
 import GooglePlaces
 
-class HomeViewController: UIViewController, GMSMapViewDelegate {
+class HomeViewController: UIViewController, GMSMapViewDelegate{
     
-    var mapView: GMSMapView?
+    @IBOutlet weak var HeatMapView: UIView!
+    @IBOutlet weak var map: GMSMapView!
     
     //view load function
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // set map @UCLA
         //TODO: Map is HARD CODED for now :(; set view to person's location later
         let camera = GMSCameraPosition.camera(withLatitude: mapLocation.coordinates.latitude, longitude: mapLocation.coordinates.longitude, zoom: 14.5)
-        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
+        map.camera = camera
+        
         
         //set markers
         //TODO: Markers are HARD CODED for now :(; change later
         setMarker(markerGeoCoords: markerCoords)
-      
-//         let marker = GMSMarker()
-//         marker.position = CLLocationCoordinate2D(latitude: 34.0522, longitude: -118.2437)
-//         marker.title = "Los Angeles"
-//         marker.snippet = "California"
-//         marker.map = mapView
-//         makeBackground();
-      
-        makeSearchButton();
-        userReportButton();
         
-        mapView?.delegate = self
-
+        setSearchButton();
+        setReportButton();
+        
+        HeatMapView.layer.cornerRadius = 20
+        HeatMapView.layer.shadowColor = UIColor.black.cgColor
+        HeatMapView.layer.shadowOpacity = 0.5
+        HeatMapView.layer.shadowOffset = .zero
+        HeatMapView.layer.shadowRadius = 10
+        map?.delegate = self
+        
     }
     
     struct data {
@@ -58,7 +56,7 @@ class HomeViewController: UIViewController, GMSMapViewDelegate {
             marker.snippet = i.address
             marker.isFlat = true //make sure the orientation of marker depends on phone
             //styling the marker
-            marker.map = self.mapView
+            marker.map = map
             marker.userData = data(image: i.image, distance: i.distance, isOpen: i.isOpen, label: i.label, pin: i.pinLabel);
             if (i.pinLabel <= 0.20) {
                 marker.icon = UIImage(named: "pin-dark-green");
@@ -76,12 +74,12 @@ class HomeViewController: UIViewController, GMSMapViewDelegate {
     
     //function that manages tapped markers
     //FIXME: function does not react at all; supposedly would if Google pin/marker is tapped
-//    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
-//        print("\n\n\ntippity tap\n\n\n")
-//        print(marker)
-////        markerTappedHandler?(marker)
-//        return true
-//    }
+    //    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+    //        print("\n\n\ntippity tap\n\n\n")
+    //        print(marker)
+    ////        markerTappedHandler?(marker)
+    //        return true
+    //    }
     
     func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
         let view = Bundle.main.loadNibNamed("CustomPopUp", owner: self, options: nil)![0] as! CustomPopUp
@@ -109,200 +107,203 @@ class HomeViewController: UIViewController, GMSMapViewDelegate {
     //will transition to location view controller once code is finished for that
     func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker)
     {
-      //  let viewController = LocationViewController()
-       // self.present(viewController, animated: true, completion: nil)
+        //  let viewController = LocationViewController()
+        // self.present(viewController, animated: true, completion: nil)
         print("pressed!")
     }
     //FIXME: make a working connection from location pg to landing pg
-//    @IBAction func unwindToLandingPG(unwindSegue: UIStoryboardSegue){
-//        print("function called")
-//    }
-   
-     // Present the Autocomplete view controller when the button is pressed.
+    //    @IBAction func unwindToLandingPG(unwindSegue: UIStoryboardSegue){
+    //        print("function called")
+    //    }
+    
+    // Present the Autocomplete view controller when the button is pressed.
     @objc func autocompleteClicked(_ sender: UIButton) {
         let autocompleteController = GMSAutocompleteViewController()
         autocompleteController.delegate = self
-
+        
         // Specify the place data types to return.
         let fields: GMSPlaceField = [.name, .placeID]
         autocompleteController.placeFields = fields
-
+        
         // Specify a filter.
         let filter = GMSAutocompleteFilter()
         filter.type = .establishment
         filter.countries = ["BR"]
         autocompleteController.autocompleteFilter = filter
-
+        
         // Display the autocomplete view controller.
         present(autocompleteController, animated: true, completion: nil)
-      }
-
+    }
+    
     func makeBackground() {
         let screenSize: CGRect = UIScreen.main.bounds
         let screenWidth = screenSize.width
         let screenHeight = screenSize.height
-
+        
         let xPos = 0
         let yPos = 470
-
+        
         let rectWidth = Int(screenWidth)
-
+        
         let rectHeight = Int(screenHeight) - 450
-
+        
         let rectFrame: CGRect = CGRect(x:CGFloat(xPos), y:CGFloat(yPos), width:CGFloat(rectWidth), height:CGFloat(rectHeight))
-
+        
         let rectView = UIView(frame: rectFrame)
-
+        
         rectView.backgroundColor = UIColor.white
-
+        
         self.view.addSubview(rectView)
-
+        
     }
-     // Add a button to the view.
-     func makeSearchButton() {
-        let btnLaunchAc = UIButton(frame: CGRect(x: 50, y: 800, width: 200, height: 35))
-       btnLaunchAc.backgroundColor = #colorLiteral(red: 0.809851794, green: 0.809851794, blue: 0.809851794, alpha: 1)
-       btnLaunchAc.setTitle("Search", for: .normal)
-       btnLaunchAc.addTarget(self, action: #selector(autocompleteClicked), for: .touchUpInside)
-       self.view.addSubview(btnLaunchAc)
-     }
-
+    
+    // Set functionality of search button
+    @IBOutlet weak var searchBtn: UIButton!
+    func setSearchButton() {
+        searchBtn.addTarget(self, action: #selector(autocompleteClicked), for: .touchUpInside)
+    }
+    
     //add user report button
     @IBOutlet weak var ReportButton: UIButton!
-    func userReportButton() {
-//        let reportBtn = UIButton(type: .custom)
-            ReportButton.frame = CGRect(x: 250, y: 800, width: 40, height: 40)
-            ReportButton.layer.cornerRadius = 0.5 * ReportButton.bounds.size.width
-            ReportButton.backgroundColor = UIColor(red: 68/255, green: 150/255, blue: 176/255, alpha: 1.0) /* #4496b0 */
-            ReportButton.clipsToBounds = true
-            ReportButton.setImage(UIImage(named:"reportBtn"), for: .normal)
-            ReportButton.addTarget(self, action: #selector(userReportButtonPressed), for: .touchUpInside)
-            view.addSubview(ReportButton)
-
+    func setReportButton() {
+        ReportButton.addTarget(self, action: #selector(userReportButtonPressed), for: .touchUpInside)
+        
     }
-
+    
     @objc func userReportButtonPressed() {
-        self.performSegue(withIdentifier: "homeToSearch", sender: self)
+        performSegue(withIdentifier: "homeToSearch", sender: self)
     }
-
-   }
+    
+    
+    @IBOutlet weak var heatMapLegend: UIImageView!
+    @IBOutlet weak var heatMapSwitch: UISwitch!
+    @IBAction func setState(_ sender: Any) {
+        if (heatMapSwitch.isOn){
+            heatMapLegend.isHidden = false;
+        }
+        else {
+            heatMapLegend.isHidden = true;
+        }
+    }
+    
+}
 
 extension HomeViewController: GMSAutocompleteViewControllerDelegate {
-
-     // Handle the user's selection.
-     func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
-       print("Place name: \(place.name ?? "none")")
-       print("Place ID: \(place.placeID ?? "none")")
+    
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: \(place.name ?? "none")")
+        print("Place ID: \(place.placeID ?? "none")")
         print("Place attributions: \(place.attributions?.string ?? "none")")
-       dismiss(animated: true, completion: nil)
-     }
-
+        dismiss(animated: true, completion: nil)
+    }
+    
     func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
         // TODO: handle the error.
         print("Error: ", error.localizedDescription)
-      }
-
-      // User canceled the operation.
-      func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+    }
+    
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
         dismiss(animated: true, completion: nil)
-      }
-
-      // Turn the network activity indicator on and off again.
-      func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
-      }
-
-      func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
-      }
+    }
     
     //slider styling functions
     func setSlider(slider:UISlider) {
-       let tgl = CAGradientLayer()
-       let frame = CGRect(x: 0.0, y: 0.0, width: slider.bounds.width, height: 9.0 )
-       tgl.frame = frame
-
+        let tgl = CAGradientLayer()
+        let frame = CGRect(x: 0.0, y: 0.0, width: slider.bounds.width, height: 9.0 )
+        tgl.frame = frame
+        
         tgl.colors = [UIColor(red: 196/255, green: 13/255, blue: 0/255, alpha: 1.0).cgColor /* #c40d00 */, UIColor(red: 255/255, green: 231/255, blue: 81/255, alpha: 1.0).cgColor /* #ffec21 */, UIColor(red: 80/255, green: 175/255, blue: 114/255, alpha: 1.0).cgColor /* #50af72 */
-
-       ]
-
-       tgl.borderWidth = 1.0
-       tgl.borderColor = UIColor.white.cgColor
-       tgl.cornerRadius = 9.0
-
-       tgl.endPoint = CGPoint(x: 1.0, y:  1.0)
-       tgl.startPoint = CGPoint(x: 0.0, y:  1.0)
-
-       UIGraphicsBeginImageContextWithOptions(tgl.frame.size, false, 0.0)
-       tgl.render(in: UIGraphicsGetCurrentContext()!)
-       let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
-       UIGraphicsEndImageContext()
-
-       slider.setMaximumTrackImage(backgroundImage?.resizableImage(withCapInsets:.zero),  for: .normal)
-       slider.setMinimumTrackImage(backgroundImage?.resizableImage(withCapInsets:.zero),  for: .normal)
-
-       let layerFrame = CGRect(x: 0, y: 0, width: 10.0, height: 10.0)
-
-       let shapeLayer = CAShapeLayer()
-       shapeLayer.path = CGPath(ellipseIn: layerFrame, transform: nil)
-       shapeLayer.fillColor = UIColor.white.cgColor
+                      
+        ]
+        
+        tgl.borderWidth = 1.0
+        tgl.borderColor = UIColor.white.cgColor
+        tgl.cornerRadius = 9.0
+        
+        tgl.endPoint = CGPoint(x: 1.0, y:  1.0)
+        tgl.startPoint = CGPoint(x: 0.0, y:  1.0)
+        
+        UIGraphicsBeginImageContextWithOptions(tgl.frame.size, false, 0.0)
+        tgl.render(in: UIGraphicsGetCurrentContext()!)
+        let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        slider.setMaximumTrackImage(backgroundImage?.resizableImage(withCapInsets:.zero),  for: .normal)
+        slider.setMinimumTrackImage(backgroundImage?.resizableImage(withCapInsets:.zero),  for: .normal)
+        
+        let layerFrame = CGRect(x: 0, y: 0, width: 10.0, height: 10.0)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = CGPath(ellipseIn: layerFrame, transform: nil)
+        shapeLayer.fillColor = UIColor.white.cgColor
         shapeLayer.strokeColor = UIColor.black.cgColor
         
-       let thumb = CALayer.init()
-       thumb.frame = layerFrame
-       thumb.addSublayer(shapeLayer)
-
-       UIGraphicsBeginImageContextWithOptions(thumb.frame.size, false, 0.0)
-
-       thumb.render(in: UIGraphicsGetCurrentContext()!)
-       let thumbImage = UIGraphicsGetImageFromCurrentImageContext()
-       UIGraphicsEndImageContext()
-
-       slider.setThumbImage(thumbImage, for: .normal)
-       slider.setThumbImage(thumbImage, for: .highlighted)
-   }
+        let thumb = CALayer.init()
+        thumb.frame = layerFrame
+        thumb.addSublayer(shapeLayer)
+        
+        UIGraphicsBeginImageContextWithOptions(thumb.frame.size, false, 0.0)
+        
+        thumb.render(in: UIGraphicsGetCurrentContext()!)
+        let thumbImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        slider.setThumbImage(thumbImage, for: .normal)
+        slider.setThumbImage(thumbImage, for: .highlighted)
+    }
     
     func setSliderInvert(slider:UISlider) {
-       let tgl = CAGradientLayer()
-       let frame = CGRect(x: 0.0, y: 0.0, width: slider.bounds.width, height: 9.0 )
-       tgl.frame = frame
-
+        let tgl = CAGradientLayer()
+        let frame = CGRect(x: 0.0, y: 0.0, width: slider.bounds.width, height: 9.0 )
+        tgl.frame = frame
+        
         tgl.colors = [UIColor(red: 80/255, green: 175/255, blue: 114/255, alpha: 1.0).cgColor /* #50af72 */, UIColor(red: 255/255, green: 231/255, blue: 81/255, alpha: 1.0).cgColor /* #ffec21 */,  UIColor(red: 196/255, green: 13/255, blue: 0/255, alpha: 1.0).cgColor /* #c40d00 */
-       ]
-
-       tgl.borderWidth = 1.0
-       tgl.borderColor = UIColor.white.cgColor
-       tgl.cornerRadius = 9.0
-
-       tgl.endPoint = CGPoint(x: 1.0, y:  1.0)
-       tgl.startPoint = CGPoint(x: 0.0, y:  1.0)
-
-       UIGraphicsBeginImageContextWithOptions(tgl.frame.size, false, 0.0)
-       tgl.render(in: UIGraphicsGetCurrentContext()!)
-       let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
-       UIGraphicsEndImageContext()
-
-       slider.setMaximumTrackImage(backgroundImage?.resizableImage(withCapInsets:.zero),  for: .normal)
-       slider.setMinimumTrackImage(backgroundImage?.resizableImage(withCapInsets:.zero),  for: .normal)
-
-       let layerFrame = CGRect(x: 0, y: 0, width: 10.0, height: 10.0)
-
-       let shapeLayer = CAShapeLayer()
-       shapeLayer.path = CGPath(ellipseIn: layerFrame, transform: nil)
-       shapeLayer.fillColor = UIColor.white.cgColor
+        ]
+        
+        tgl.borderWidth = 1.0
+        tgl.borderColor = UIColor.white.cgColor
+        tgl.cornerRadius = 9.0
+        
+        tgl.endPoint = CGPoint(x: 1.0, y:  1.0)
+        tgl.startPoint = CGPoint(x: 0.0, y:  1.0)
+        
+        UIGraphicsBeginImageContextWithOptions(tgl.frame.size, false, 0.0)
+        tgl.render(in: UIGraphicsGetCurrentContext()!)
+        let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        slider.setMaximumTrackImage(backgroundImage?.resizableImage(withCapInsets:.zero),  for: .normal)
+        slider.setMinimumTrackImage(backgroundImage?.resizableImage(withCapInsets:.zero),  for: .normal)
+        
+        let layerFrame = CGRect(x: 0, y: 0, width: 10.0, height: 10.0)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = CGPath(ellipseIn: layerFrame, transform: nil)
+        shapeLayer.fillColor = UIColor.white.cgColor
         shapeLayer.strokeColor = UIColor.black.cgColor
         
-       let thumb = CALayer.init()
-       thumb.frame = layerFrame
-       thumb.addSublayer(shapeLayer)
-
-       UIGraphicsBeginImageContextWithOptions(thumb.frame.size, false, 0.0)
-
-       thumb.render(in: UIGraphicsGetCurrentContext()!)
-       let thumbImage = UIGraphicsGetImageFromCurrentImageContext()
-       UIGraphicsEndImageContext()
-
-       slider.setThumbImage(thumbImage, for: .normal)
-       slider.setThumbImage(thumbImage, for: .highlighted)
-        }
-   }
+        let thumb = CALayer.init()
+        thumb.frame = layerFrame
+        thumb.addSublayer(shapeLayer)
+        
+        UIGraphicsBeginImageContextWithOptions(thumb.frame.size, false, 0.0)
+        
+        thumb.render(in: UIGraphicsGetCurrentContext()!)
+        let thumbImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        slider.setThumbImage(thumbImage, for: .normal)
+        slider.setThumbImage(thumbImage, for: .highlighted)
+    }
+}
