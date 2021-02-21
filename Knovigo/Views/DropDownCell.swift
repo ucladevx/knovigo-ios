e428c40a9b1e38af84032597d9cafea4341b94cc
Jeff848 <jeffreyma.wii@gmail.com>
2021-02-21 04:59:07 -0800

    Added filters with dummy filter capabilities

---
+++ Knovigo/Views/DropDownCell.swift
@@ -0,0 +1,25 @@
+//
+//  FilterViewCell.swift
+//  Knovigo
+//
+//  Created by Jeffrey Ma on 2/16/21.
+//
+
+import UIKit
+import DropDown
+
+class FilterViewCell: DropDownCell {
+    @IBOutlet var toggleswitch: UISwitch!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        // Configure the view for the selected state
+    }
+    
+    
+    
+}
